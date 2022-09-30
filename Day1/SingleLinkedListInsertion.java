7d95e13f94ea80a6873652bb665d8e31b874c12a
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2022-09-30 22:39:24 +0530

    initial commit

--- Day1/SingleLinkedListInsertion.java
+++ Day1/SingleLinkedListInsertion.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // Day1
 // I am Learning Singly Linked List Insertion Operation in different scenerio.
 package Day1;
@@ -93,3 +94,100 @@ public class SingleLinkedListInsertion {
         s1.display();
     }
 }
+=======
+// Day1
+// I am Learning Singly Linked List Insertion Operation in different scenerio.
+package Day1;
+
+public class SingleLinkedListInsertion {
+    class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public Node head = null;
+    public Node tail = null;
+
+    public void addBegNode(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            Node temp = head;
+            head = newNode;
+            head.next = temp;
+        }
+    }
+
+    public void addLastNode(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public void addBetweenNode(int data, int position) {
+        Node newNode = new Node(data);
+        Node previous = head;
+        int count = 1;
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            while (count < position - 1) {
+                previous = previous.next;
+                count++;
+            }
+            Node current = previous.next;
+            newNode.next = current;
+            previous.next = newNode;
+        }
+    }
+
+    public void display() {
+        Node current = head;
+        if (head == null) {
+            System.out.println("Empty");
+            return;
+        }
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        SingleLinkedListInsertion s1 = new SingleLinkedListInsertion();
+        s1.addBegNode(2);
+        s1.addBegNode(3);
+        s1.addBegNode(4);
+        s1.addBegNode(6);
+        s1.addBegNode(8);
+        System.out.println("After Insetion:");
+        s1.display();
+        System.out.println();
+        System.out.println("Inseting node at the beginning:");
+        s1.addBegNode(11);
+        s1.display();
+        System.out.println();
+        System.out.println("Inseting node at the particular Location:");
+        s1.addBetweenNode(12, 2);
+        s1.display();
+        System.out.println();
+        System.out.println("Inseting node at the Last:");
+        s1.addLastNode(34);
+        s1.display();
+    }
+}
+>>>>>>> Stashed changes
--- README.md
+++ README.md
@@ -7,6 +7,10 @@ More Info [here][Day1/SingleLinkedListInsertion.java]
 # Day2
 I am Learning Singly Linked List Deletion Operation in different scenerio.
 
+<<<<<<< Updated upstream
 More Info [here][Day2/SingleLinkedListDeletion.java]
 
+=======
+More Info [here][Day1/SingleLinkedListDeletio.java]
+>>>>>>> Stashed changes
 
