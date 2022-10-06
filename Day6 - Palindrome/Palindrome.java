63b31e9eff454aacda4f0924f7ccc14c27dab1a5
Niraj Kumar Tiwari <63774724+NirajKrTiwari@users.noreply.github.com>
2022-10-06 21:21:57 +0530

    Update Palindrome.java

--- Day6 - Palindrome/Palindrome.java
+++ Day6 - Palindrome/Palindrome.java
@@ -1,23 +1,4 @@
-<<<<<<< HEAD
-import java.util.Scanner;
-public class PalindromeGeek {
-        public static void main(String[] args) {
-            Scanner in=new Scanner(System.in);
-            System.out.println("Enter any String: ");
-            String S=in.nextLine();
-            String s1 = "";
-            for (int i = S.length() - 1; i >= 0; i--) {
-                s1 = s1 + S.charAt(i);
-            }
-            if (S.equals(s1)) {
-                System.out.println("Palindrome");
-            } else {
-                System.out.println("Not Palindrome");
-            }
 
-        }
-    }
-=======
 import java.util.Scanner;
 public class PalindromeGeek {
         public static void main(String[] args) {
@@ -36,4 +17,3 @@ public class PalindromeGeek {
 
         }
     }
->>>>>>> f867c881cc166f3eb14d6706dbc8aca8fd1573d7
