42beedd15895d7eeeb11f33c20001ca4c922feb2
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2022-10-06 21:07:44 +0530

    initial commit

--- Day9 - Word Occurrence/Word Occurrence.java
+++ Day9 - Word Occurrence/Word Occurrence.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public class DisplayFirstRepeatingChar {
 
         static char firstRep(String S)
@@ -23,29 +22,3 @@ public class DisplayFirstRepeatingChar {
     }
 
 }
-=======
-public class DisplayFirstRepeatingChar {
-
-        static char firstRep(String S)
-        {
-            for(int i=0;i<S.length();i++)
-            {
-                for(int j=i+1;j<S.length();j++)
-                {
-                    if(S.charAt(i)==S.charAt(j))
-                    {
-                        return S.charAt(i);
-                    }
-                }
-            }
-            return '#';
-        }
-
-    public static void main(String[] args) {
-        String S="geeksofgeeks";
-
-        System.out.println(firstRep(S));
-    }
-
-}
->>>>>>> f867c881cc166f3eb14d6706dbc8aca8fd1573d7
