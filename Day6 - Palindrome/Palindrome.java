4c2b1a9685b3f7da20f1dc3c2e0fdd6bd05210dd
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2022-10-06 22:13:18 +0530

    initial commit

--- Day6 - Palindrome/Palindrome.java
+++ Day6 - Palindrome/Palindrome.java
@@ -1,39 +1,39 @@
-<<<<<<< HEAD
-import java.util.Scanner;
-public class PalindromeGeek {
-        public static void main(String[] args) {
-            Scanner in=new Scanner(System.in);
-            System.out.println("Enter any String: ");
-            String S=in.nextLine();
-            String s1 = "";
-            for (int i = S.length() - 1; i >= 0; i--) {
-                s1 = s1 + S.charAt(i);
-            }
-            if (S.equals(s1)) {
-                System.out.println("Palindrome");
-            } else {
-                System.out.println("Not Palindrome");
-            }
 
-        }
-    }
-=======
-import java.util.Scanner;
-public class PalindromeGeek {
-        public static void main(String[] args) {
-            Scanner in=new Scanner(System.in);
-            System.out.println("Enter any String: ");
-            String S=in.nextLine();
-            String s1 = "";
-            for (int i = S.length() - 1; i >= 0; i--) {
-                s1 = s1 + S.charAt(i);
-            }
-            if (S.equals(s1)) {
-                System.out.println("S is Palindrome");
-            } else {
-                System.out.println("S is not Palindrome");
-            }
+/*
+ Given a string S, check if it is palindrome or not.
 
+Example 1:
+
+Input: S = "abba"
+Output: 1
+Explanation: S is a palindrome
+
+Example 2:
+
+Input: S = "abc" 
+Output: 0
+Explanation: S is not a palindrome
+
+Expected Time Complexity: O(Length of S)
+Expected Auxiliary Space: O(1)
+
+ */
+import java.util.Scanner;
+
+public class PalindromeGeek {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter any String: ");
+        String S = in.nextLine();
+        String s1 = "";
+        for (int i = S.length() - 1; i >= 0; i--) {
+            s1 = s1 + S.charAt(i);
         }
+        if (S.equals(s1)) {
+            System.out.println("S is Palindrome");
+        } else {
+            System.out.println("S is not Palindrome");
+        }
+
     }
->>>>>>> f867c881cc166f3eb14d6706dbc8aca8fd1573d7
+}
--- Day7 - String Reverse/StringReverse.java
+++ Day7 - String Reverse/StringReverse.java
@@ -1,37 +1,34 @@
-<<<<<<< HEAD
+/*
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+
+
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+ 
+ */
 public class StringReverse {
     public static void main(String[] args) {
-        char[] s = {'h','e','l','l','o'};
-        int j=0,k=s.length-1;
-        for(int i=(s.length/2)-1;i>=0;i--)
-        {
-            char temp=s[k];
-            s[k]=s[j];
-            s[j]=temp;
-            j++;k--;
+        char[] s = { 'h', 'e', 'l', 'l', 'o' };
+        int j = 0, k = s.length - 1;
+        for (int i = (s.length / 2) - 1; i >= 0; i--) {
+            char temp = s[k];
+            s[k] = s[j];
+            s[j] = temp;
+            j++;
+            k--;
         }
-        for(int i=0;i<s.length;i++)
-        {
+        for (int i = 0; i < s.length; i++) {
             System.out.print(s[i]);
         }
     }
 }
-=======
-public class StringReverse {
-    public static void main(String[] args) {
-        char[] s = {'h','e','l','l','o'};
-        int j=0,k=s.length-1;
-        for(int i=(s.length/2)-1;i>=0;i--)
-        {
-            char temp=s[k];
-            s[k]=s[j];
-            s[j]=temp;
-            j++;k--;
-        }
-        for(int i=0;i<s.length;i++)
-        {
-            System.out.print(s[i]);
-        }
-    }
-}
->>>>>>> f867c881cc166f3eb14d6706dbc8aca8fd1573d7
