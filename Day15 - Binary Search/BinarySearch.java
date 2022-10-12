43d7d8c41a15d9f16b10cd74ee119bf80cea5d05
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2022-10-13 00:14:59 +0530

    Create BinarySearch.java

---
+++ Day15 - Binary Search/BinarySearch.java
@@ -0,0 +1,51 @@
+/*
+Binary Search
+
+Enter number of element you want to enter:4
+Enter elements
+12
+23
+56
+78
+Enter Number You want to Search: 23
+23 is found at index: 1
+ */
+*/
+import java.util.Scanner;
+public class BinarySearch {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter number of element you want to enter:");
+        int n=in.nextInt();
+        int a[]=new int[n];
+        System.out.println("Enter elements");
+        for(int i=0;i<n;i++)
+        {
+            a[i]=in.nextInt();
+        }
+        System.out.print("Enter Number You want to Search: ");
+        int target = in.nextInt();
+        System.out.println(target + " is found at index: " + binarySearch(a, target));
+    }
+    public static int binarySearch(int[] a, int target) {
+        int lo=0,mid=0;
+        int hi=a.length-1;
+        while(lo<=hi)
+        {
+            mid=(lo+hi)/2;
+            if(a[mid]==target)
+            {
+                return mid;
+            }
+            else if(a[mid]<target)
+            {
+                lo=mid+1;
+            }
+            else
+            {
+                hi=mid-1;
+            }
+        }
+        return -1;
+    }
+}
